a603f8b99744f6f2cacf4db4ac9981dd3750c664
ivy <ivy@not-ivy.dev>
2022-12-24 00:34:02 -0800

    refactor: use swc and remove tsx

---
+++ spack.config.js
@@ -0,0 +1,16 @@
+const { config } = require("@swc/core/spack");
+
+module.exports = config({
+	entry: {
+		index: `${__dirname}/src/index.ts`,
+	},
+	output: {
+		path: `${__dirname}/dist`,
+	},
+	options: {
+		minify: true,
+		jsc: {
+			target: "es2016",
+		},
+	},
+});
